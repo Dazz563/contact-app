4bfb7db6d75d064df371d454427efee938a7ba1d
Darren <58746026+Dazz563@users.noreply.github.com>
2023-02-11 04:17:34 +0200

    blade updates and router updates

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'Contact App | Add new contact')
+@section('title', 'Contact App | Contact ' . $contact['name'])
 
 @section('content')
     <!-- content -->
@@ -10,63 +10,56 @@
             <div class="col-md-8">
               <div class="card">
                 <div class="card-header card-title">
-                  <strong>Add New Contact</strong>
+                  <strong>Contact Details</strong>
                 </div>           
                 <div class="card-body">
                   <div class="row">
                     <div class="col-md-12">
                       <div class="form-group row">
-                        <label for="first_name" class="col-md-3 col-form-label">First Name</label>
+                        <label for="first_name" class="col-md-3 col-form-label">Name</label>
                         <div class="col-md-9">
-                          <input type="text" name="first_name" id="first_name" class="form-control is-invalid">
-                          <div class="invalid-feedback">
-                            Please choose a username.
-                          </div>
+                          <p class="form-control-plaintext text-muted">{{ $contact['name'] }}</p>
                         </div>
                       </div>
   
                       <div class="form-group row">
-                        <label for="last_name" class="col-md-3 col-form-label">Last Name</label>
+                        <label for="last_name" class="col-md-3 col-form-label">Phone</label>
                         <div class="col-md-9">
-                          <input type="text" name="last_name" id="last_name" class="form-control">
+                          <p class="form-control-plaintext text-muted">{{ $contact['phone'] }}</p>
                         </div>
                       </div>
   
                       <div class="form-group row">
                         <label for="email" class="col-md-3 col-form-label">Email</label>
                         <div class="col-md-9">
-                          <input type="text" name="email" id="email" class="form-control">
+                          <p class="form-control-plaintext text-muted">alfred@test.com</p>
                         </div>
                       </div>
   
                       <div class="form-group row">
                         <label for="phone" class="col-md-3 col-form-label">Phone</label>
                         <div class="col-md-9">
-                          <input type="text" name="phone" id="phone" class="form-control">
+                          <p class="form-control-plaintext text-muted">+6286767565656</p>
                         </div>
                       </div>
   
                       <div class="form-group row">
                         <label for="name" class="col-md-3 col-form-label">Address</label>
                         <div class="col-md-9">
-                          <textarea name="address" id="address" rows="3" class="form-control"></textarea>
+                          <p class="form-control-plaintext text-muted">Lorem ipsum dolor</p>
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="company_id" class="col-md-3 col-form-label">Company</label>
                         <div class="col-md-9">
-                          <select name="company_id" id="company_id" class="form-control">
-                            <option value="">Select Company</option>
-                            <option value="1">Company One</option>
-                            <option value="2">Company Two</option>
-                            <option value="3">Company Three</option>
-                          </select>
+                          <p class="form-control-plaintext text-muted">Company One</p>
                         </div>
                       </div>
                       <hr>
                       <div class="form-group row mb-0">
                         <div class="col-md-9 offset-md-3">
-                            <button type="submit" class="btn btn-primary">Save</button>
+                            <a href="#" class="btn btn-info">Edit</a>
+                            <a href="#" class="btn btn-outline-danger">Delete</a>
                             <a href="{{ route('contacts.index') }}" class="btn btn-outline-secondary">Cancel</a>
                         </div>
                       </div>
@@ -79,4 +72,3 @@
         </div>
       </main>
 @endsection
-
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,17 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', function() {
-    return view('contacts.index');
-})->name('contacts.index');
+Route::controller(ContactController::class)->group(function() {
+    Route::get('/contacts', 'index')->name('contacts.index');
+    Route::get('/contacts/create', 'create')->name('contacts.create');
+    Route::get('/contacts/{id}', 'show')->name('contacts.show');
+});
 
-Route::get('/contacts/create', function() {
-    return "<h1>Add new contact</h1>";
-})->name('contacts.create');
-
-Route::get('/contacts/{id}', function($id) {
-    return "Contact " . $id;
-})->name('contacts.show');
 
 Route::fallback(function() {
     return "<h1>Sorry, the page does not exist</h1>";
